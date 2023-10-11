a57e6ec004095e18bdc0d4d17a8770042b403fd1
manidyv5 <146942814+manidyv5@users.noreply.github.com>
2023-10-11 13:00:13 +0530

    Update challenge 3.2.py

--- challenge 3.2.py
+++ challenge 3.2.py
@@ -1,17 +1,27 @@
 class Student:
-  def __init__(self, name, roll_number, cgpa):
-    self.name = name
-    self.roll_number = roll_number
-    self.cgpa = cgpa
-def sort_Students(Student_list):
-  sorted_Students = sorted(Student_list,key=lambda Student: Student.cgpa,reverse=True)
-  return sorted_Students
-Students = [
-    Student("Hari", "A123", 7.8),
-    Student("Abi", "A124", 8.1),
-    Student("Dhinesh", "A125", 9.0),
-    Student("Anu", "A126", 8.6)
-]
-sorted_Students = sort_Students(Students)
-for Students in sorted_Students:
-  print("Name: {}, Roll Number: {}, CGPA:{}".format(Student.name,Student.roll_number,Student.cgpa))
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
+
+def sort_students(student_list):
+    # Sort the list of students based on CGPA in descending order
+    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
+    return sorted_students
+
+# Example usage:
+if __name__ == "__main__":
+    # Create a list of student objects
+    students = [
+        Student("Alice", "001", 3.8),
+        Student("Bob", "002", 3.5),
+        Student("Charlie", "003", 3.9),
+        Student("David", "004", 3.7),
+    ]
+
+    # Sort the list of students by CGPA
+    sorted_students = sort_students(students)
+
+    # Print the sorted list
+    for student in sorted_students:
+        print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
